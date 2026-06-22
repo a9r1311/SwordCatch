06e90bfacef6dd9be0ad3e202ad52522d55b149e
a9r1311 <a9r1311@outlook.jp>
2026-06-22 09:17:25 +0900

    FPSSetter変更
    ・独自のフレーム計算を廃止。
    ・シングルトンを廃止

---
+++ Assets/Script/Core/Other/FPSSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Kamatte.Core
+{
+    [DisallowMultipleComponent]
+    public sealed class FPSSetter : MonoBehaviour    //  FPSをセットする
+    {
+        [SerializeField, Range(30, 120)] private int _targetFPS = 60;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = _targetFPS;
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
